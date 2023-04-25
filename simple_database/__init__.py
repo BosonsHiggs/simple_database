dd357d675c5de44384979f0b5501b16a529de5d6
iagolirapassos <iagolirapassos@gmail.com>
2023-04-25 18:59:03 -0300

    __init__ update

--- simple_database/__init__.py
+++ simple_database/__init__.py
@@ -1,2 +1,10 @@
 from .simple_database import SimpleDatabase
 from .async_simple_database import AsyncSimpleDatabase
+from . import (
+    backup,
+    index,
+    performance,
+    replication,
+    security,
+    transaction
+)
